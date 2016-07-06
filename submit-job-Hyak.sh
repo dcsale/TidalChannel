30628502c93495227cb251f0c218af988ed3a61c
Danny Clay Sale <dsale@uw.edu>
2016-07-05 20:12:31 -0700

    update PBS to run on Hyak

--- submit-job-Hyak.sh
+++ submit-job-Hyak.sh
@@ -12,7 +12,7 @@
 ## --------------------------------------------------------
 ## RENAME for your job
 ## --------------------------------------------------------
-#PBS -N big_v4-motley
+#PBS -N tidalchannel-motley
 
 
 ## --------------------------------------------------------
@@ -26,10 +26,11 @@
 ## --------------------------------------------------------
 ## NUMBER nodes, CPUs per node, and MEMORY
 ## --------------------------------------------------------
-## PBS -l nodes=1:ppn=16,mem=55gb,feature=intel
-## PBS -l nodes=2:ppn=16,mem=60gb,feature=intel
-#PBS -l nodes=3:ppn=16,mem=150gb,feature=intel
+#PBS -l nodes=1:ppn=16,mem=50gb,feature=intel
+## PBS -l nodes=2:ppn=16,mem=100gb,feature=intel
+## PBS -l nodes=3:ppn=16,mem=150gb,feature=intel
 ## PBS -l nodes=4:ppn=16,mem=200gb,feature=intel
+## PBS -l nodes=6:ppn=16,mem=300gb,feature=intel
 ## PBS -l nodes=8:ppn=16,mem=440gb,feature=intel
 ## PBS -l nodes=16:ppn=16,mem=60gb,feature=intel
 
@@ -43,8 +44,10 @@
 ## --------------------------------------------------------
 ## LOG the (stderr and stdout) job output in the directory
 ## --------------------------------------------------------
-## PBS -j oe -o /gscratch/motley/dsale/job_output/logs
-#PBS -j oe -o /gscratch/stf/dsale/mets2016
+## PBS -j oe -o /gscratch/motley/dsale/logs
+## PBS -j oe -o /gscratch/stf/dsale/mets2016
+#PBS -j oe -o /suppscr/fluids/danny/logs
+## PBS -j oe -o /gscratch/stf/dsale/logs
 
 ## --------------------------------------------------------
 ## EMAIL to send when job is aborted, begins, and terminates
@@ -68,7 +71,7 @@ module load contrib/starccm_11.02.010-R8
 
 #starSimFile="mets2016__Big__Backfill"
 # starSimFile="runs.mets2016__Big__Backfill"
-starSimFile="test"
+starSimFile="tidalchannel-hyak-test"
 
 # if running in the backfill, account for checkpointing and restarting (actually, maybe do not need?)
 # logFile="log.*"
@@ -82,7 +85,7 @@ starSimFile="test"
 #     starMacros="main.java"
 # fi
 
-starMacros="./macros/_main_0.java"
+starMacros="../macros/_main_0.java"
 
 ## CHANGE directory to where job was submitted (careful, PBS defaults to user home directory)
 cd $PBS_O_WORKDIR
@@ -91,13 +94,13 @@ cd $PBS_O_WORKDIR
 ## and rename file used for restart after checkpointing (this file gets big) 
 # cp --no-clobber $starSimFile.sim runs_$starSimFile.sim
 mkdir ./cases
-cp --no-clobber initial.sim ./cases/runs_$starSimFile.sim
+cp --no-clobber initial.sim ./cases/$starSimFile.sim
 cd ./cases/
 #rm log.*
 #rm *.sim~
 
 ## RUN my simulation file in batch mode
 # on Hyak
-starccm+ -batch $starMacros -np ${PBS_NP} -machinefile ${PBS_NODEFILE} -licpath 1999@mgmt2.hyak.local -batch-report runs_$starSimFile.sim 2>&1 | tee log.$starSimFile
+starccm+ -batch $starMacros -np ${PBS_NP} -machinefile ${PBS_NODEFILE} -licpath 1999@mgmt2.hyak.local -batch-report $starSimFile.sim 2>&1 | tee log.$starSimFile
 # on local
-#starccm+ -batch $starMacros -np 16 -licpath 1999@lmas.engr.washington.edu -batch-report runs_$starSimFile.sim 2>&1 | tee log.$starSimFile
+#starccm+ -batch $starMacros -np 16 -licpath 1999@lmas.engr.washington.edu -batch-report $starSimFile.sim 2>&1 | tee log.$starSimFile
